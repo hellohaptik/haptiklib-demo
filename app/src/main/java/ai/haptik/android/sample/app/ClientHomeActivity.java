671f606d9d068dcf317e013899e89b85edbd44b2
rajas.ashtikar <rajas.ashtikar@haptik.co>
2020-11-05 18:53:16 +0530

    Update demo app to latest version of Android SDK
    - Update the Haptik Android SDK version to 7.0.0
    - Update the min SDK version to 21
    - Migrated the app to Androidx
    - Remove unwanted code from AndroidManifest
    - Update the version of Gradle Wrapper
    
    CIA-6928

--- app/src/main/java/ai/haptik/android/sample/app/ClientHomeActivity.java
+++ app/src/main/java/ai/haptik/android/sample/app/ClientHomeActivity.java
@@ -5,17 +5,15 @@ import ai.haptik.android.sdk.HaptikException;
 import ai.haptik.android.sdk.HaptikLib;
 import ai.haptik.android.sdk.Router;
 import ai.haptik.android.sdk.SignUpData;
-import ai.haptik.android.sdk.data.local.models.TaskModel;
 import ai.haptik.android.sdk.messaging.MessagingClient;
 import ai.haptik.android.sdk.messaging.MessagingEventListener;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ClientHomeActivity extends AppCompatActivity {
 
